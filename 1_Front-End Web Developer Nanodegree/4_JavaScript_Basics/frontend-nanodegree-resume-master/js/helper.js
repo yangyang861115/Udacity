1ec8bf54db86a9fc546d0cecdbb2547d80a7ba6f
yangyang861115 <yangyang861115@gmail.com>
2016-04-26 16:17:49 -0400

    finished course 4

--- 1_Front-End Web Developer Nanodegree/4_JavaScript_Basics/frontend-nanodegree-resume-master/js/helper.js
+++ 1_Front-End Web Developer Nanodegree/4_JavaScript_Basics/frontend-nanodegree-resume-master/js/helper.js
@@ -40,7 +40,7 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img class="pjimg" src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -62,11 +62,12 @@ var googleMap = '<div id="map"></div>';
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName(name) || function(){};
-    $('#name').html(iName);  
-  });
+$(document).ready(function () {
+    $('button').click(function () {
+        var name = bio.name;
+        var iName = inName(name) || function () {};
+        $('#name').html(iName);
+    });
 });
 
 /*
@@ -74,18 +75,17 @@ The next few lines about clicks are for the Collecting Click Locations quiz in L
 */
 clickLocations = [];
 
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
+function logClicks(x, y) {
+    clickLocations.push({
+        x: x,
+        y: y
+    });
+    console.log('x location: ' + x + '; y location: ' + y);
 }
 
-$(document).click(function(loc) {
-  // your code goes here!
+$(document).click(function (loc) {
+    // your code goes here!
+    logClicks(loc.pageX, loc.pageY);
 });
 
 
@@ -95,7 +95,7 @@ This is the fun part. Here's where we generate the custom Google Map for the web
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
+var map; // declares a global map variable
 
 
 /*
@@ -103,128 +103,137 @@ Start here! initializeMap() is called when page is loaded.
 */
 function initializeMap() {
 
-  var locations;
+    var locations;
 
-  var mapOptions = {
-    disableDefaultUI: true
-  };
+    var mapOptions = {
+        disableDefaultUI: true
+    };
 
-  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
-  // <div id="map">, which is appended as part of an exercise late in the course.
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+    // This next line makes `map` a new Google Map JavaScript Object and attaches it to
+    // <div id="map">, which is appended as part of an exercise late in the course.
+    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
-  function locationFinder() {
+    /*
+    locationFinder() returns an array of every location string from the JSONs
+    written for bio, education, and work.
+    */
+    function locationFinder() {
 
-    // initializes an empty array
-    var locations = [];
+        // initializes an empty array
+        var locations = [];
 
-    // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+        // adds the single location property from bio to the locations array
+        locations.push(bio.contact_info.location);
 
-    // iterates through school locations and appends each location to
+        // iterates through school locations and appends each location to
+        // the locations array
+        for (var school in education.schools) {
+            locations.push(education.schools[school].city);
+        }
+
+        // iterates through work locations and appends each location to
+        // the locations array
+        for (var job in work.jobs) {
+            locations.push(work.jobs[job].city);
+        }
+        console.log(unique(locations));
+        return unique(locations);
+    }
+
+    function unique(list) {
+        var result = [];
+        $.each(list, function (i, e) {
+            if ($.inArray(e, result) == -1) result.push(e);
+        });
+        return result;
+    }
+
+    /*
+    createMapMarker(placeData) reads Google Places search results to create map pins.
+    placeData is the object returned from search results containing information
+    about a single location.
+    */
+    function createMapMarker(placeData) {
+
+        // The next lines save location data from the search result object to local variables
+        var lat = placeData.geometry.location.lat(); // latitude from the place service
+        var lon = placeData.geometry.location.lng(); // longitude from the place service
+        var name = placeData.formatted_address; // name of the place from the place service
+        var bounds = window.mapBounds; // current boundaries of the map window
+
+        // marker is an object with additional data about the pin for a single location
+        var marker = new google.maps.Marker({
+            map: map,
+            position: placeData.geometry.location,
+            title: name
+        });
+
+        // infoWindows are the little helper windows that open when you click
+        // or hover over a pin on a map. They usually contain more information
+        // about a location.
+        var infoWindow = new google.maps.InfoWindow({
+            content: name
+        });
+
+        // hmmmm, I wonder what this is about...
+        google.maps.event.addListener(marker, 'click', function () {
+            // your code goes here!
+            infoWindow.open(map, marker);
+        });
+
+        // this is where the pin actually gets added to the map.
+        // bounds.extend() takes in a map location object
+        bounds.extend(new google.maps.LatLng(lat, lon));
+        // fit the map to the new marker
+        map.fitBounds(bounds);
+        // center the map
+        map.setCenter(bounds.getCenter());
+    }
+
+    /*
+    callback(results, status) makes sure the search returned results for a location.
+    If so, it creates a new map marker for that location.
+    */
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            createMapMarker(results[0]);
+        }
+    }
+
+    /*
+    pinPoster(locations) takes in the array of locations created by locationFinder()
+    and fires off Google place searches for each location
+    */
+    function pinPoster(locations) {
+
+        // creates a Google place search service object. PlacesService does the work of
+        // actually searching for location data.
+        var service = new google.maps.places.PlacesService(map);
+
+        // Iterates through the array of locations, creates a search object for each location
+        for (var place in locations) {
+
+            // the search request object
+            var request = {
+                query: locations[place]
+            };
+
+            // Actually searches the Google Maps API for location data and runs the callback
+            // function with the search results after each search.
+            service.textSearch(request, callback);
+        }
+    }
+
+    // Sets the boundaries of the map based on pin locations
+    window.mapBounds = new google.maps.LatLngBounds();
+
+    // locations is an array of location strings returned from locationFinder()
+    locations = locationFinder();
+
+    // pinPoster(locations) creates pins on the map for each location in
     // the locations array
-    for (var school in education.schools) {
-      locations.push(education.schools[school].location);
-    }
-
-    // iterates through work locations and appends each location to
-    // the locations array
-    for (var job in work.jobs) {
-      locations.push(work.jobs[job].location);
-    }
-
-    return locations;
-  }
-
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
-
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.k;  // latitude from the place service
-    var lon = placeData.geometry.location.D;  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
-
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name
-    });
-
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
-
-    // hmmmm, I wonder what this is about...
-    google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
-    });
-
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
-    map.fitBounds(bounds);
-    // center the map
-    map.setCenter(bounds.getCenter());
-  }
-
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      createMapMarker(results[0]);
-    }
-  }
-
-  /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
-  function pinPoster(locations) {
-
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
-    var service = new google.maps.places.PlacesService(map);
-
-    // Iterates through the array of locations, creates a search object for each location
-    for (var place in locations) {
-
-      // the search request object
-      var request = {
-        query: locations[place]
-      };
-
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
-      service.textSearch(request, callback);
-    }
-  }
-
-  // Sets the boundaries of the map based on pin locations
-  window.mapBounds = new google.maps.LatLngBounds();
-
-  // locations is an array of location strings returned from locationFinder()
-  locations = locationFinder();
-
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
-  pinPoster(locations);
+    pinPoster(locations);
 
 }
 
@@ -233,11 +242,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function (e) {
+    //Make sure the map bounds get updated on page resize
+    map.fitBounds(mapBounds);
+});
--- 1_Front-End Web Developer Nanodegree/4_JavaScript_Basics/frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ 1_Front-End Web Developer Nanodegree/4_JavaScript_Basics/frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -1,1 +1,220 @@
-$("#main").append("Yang Yang");
+//$("#main").append("Yang Yang");
+//[string].replace([old], [new]);
+//var formattedName = HTMLheaderName.replace("%data%", "Yang Yang");
+//var formattedRole = HTMLheaderRole.replace("%data%", "MIS Student");
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+//s.toUpperCase()  s.slice(start, [end]) s.join(" ") s.split(" ") 
+//arr.slice(start, [end])   arr.length arr.pop() arr.push()
+var charEscape = function (_html) {
+    var newHTML = _html;
+
+    newHTML = _html.replace(/</g, "&lt;");
+    newHTML = newHTML.replace(/>/g, "&gt;");
+
+    return newHTML;
+};
+
+
+//bio information
+var bio = {
+    "name": "Yang Yang",
+    "role": "MIS Student",
+    "contact_info": {
+        "mobile": "812-3613408",
+        "email": "yangyang861115@gmail.com",
+        "github": "yangyang861115",
+        "location": "San Jose, CA",
+        "blog": "www.yangyangsite.com"
+    },
+    "bioPic": "images/me.png",
+    "message": "Welcome to my page!",
+    "skills": ["HTML", "CSS", "JavaScript", "JQuery", "Bootstrap", "AngularJS", "PHP", "SQL", "Unix", "Python", "Java", "Github"]
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.message);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+
+
+var formattedContactGeneric = HTMLcontactGeneric.replace("%data%", bio.contact_info.email)
+    .replace("%contact%", "general contact");
+$("#topContacts").append(formattedContactGeneric);
+
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact_info.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contact_info.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contact_info.github);
+var formattedBlog = HTMLblog.replace("%data%", bio.contact_info.blog);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact_info.location);
+var contacts = formattedMobile+ formattedEmail + formattedGithub + formattedBlog+formattedLocation;
+$("#topContacts").append(contacts);
+$("#footerContacts").append(contacts);
+
+
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    for (var idx in bio.skills) {
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[idx]);
+        $("#skills").append(formattedSkills);
+    }
+
+}
+
+//work experience
+var work = {
+    "jobs": [
+        {
+            "job_description": "Upgraded an old version website for event registrations into a single page web application with a new protocol that uses Model View Controller architecture by AngularJS, Bootstrap and PHP. Upgraded the website into a responsive and user friendly website by using Bootstrap and JQuery",
+            "employer": "Campus Crusade for Christ (Cru)",
+            "starts": "December 2015",
+            "years": "6 months",
+            "city": "Bloomington, IN",
+            "title": "Web Developer"
+    },
+        {
+            "job_description": "Assist students and faculty with using technology equipment, troubleshoot user problems. Maintain the Information Commons space",
+            "employer": "School of Informatics and Computing, Indiana University Bloomington",
+            "starts": "January 2015",
+            "years": "1.5 years",
+            "city": "Bloomington, IN",
+            "title": "ILS Technology Consultant"
+    },
+        {
+            "job_description": "Developed official website for IU SALSA lab by using HTML, CSS, JavaScript. http://salsaproj.indiana.edu/Salsahpc. Helped web page owner evaluate, update, and maintain web content",
+            "employer": "School of Informatics and Computing, Indiana University Bloomington",
+            "starts": "January 2015",
+            "years": "6 months",
+            "city": "Bloomington, IN",
+            "title": "Web Front End Developer"
+    },
+        {
+            "job_description": "Provided financial services to banking customers. Collected customer feedback and made process changes to exceed customer satisfaction goals",
+            "employer": "Industrial and Commercial Bank",
+            "starts": "August 2009",
+            "years": "2 years",
+            "city": "Suzhou, Jiangsu, China",
+            "title": "Banker"
+    }
+
+]
+};
+
+function displayWork() {
+    for (var i in work.jobs) {
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+        var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+        var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[i].years);
+        var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].city);
+        var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].job_description);
+        var job = formattedEmployer + formattedWorkTitle + formattedWorkDates + formattedWorkLocation + formattedWorkDescription;
+
+        $("#workExperience").append(HTMLworkStart);
+        $(".work-entry:last").append(job);
+        //$("#workExperience .work-entry").last().html(job);
+    }
+}
+displayWork();
+
+//education
+
+var education = {
+    "schools": [
+        {
+            "schoolName": "Indiana University Bloomington",
+            "dates": "January 2015 - July 2016",
+            "degree": "Master",
+            "major": "Information Science",
+            "city": "Bloomington, IN"
+
+        }, {
+            "schoolName": "Nanjing Audit University",
+            "dates": "August 2005 - July 2009",
+            "degree": "Bachelor",
+            "major": "Auditing",
+            "city": "Nanjing, Jiangsu, China"
+        }],
+    "onlineCourses": [
+        {
+            "title": "JavaScript Crash Course",
+            "school": "Udacity",
+            "dates": "2016",
+            "url": "https://classroom.udacity.com/courses/ud804"
+        }
+    ]
+};
+
+for (var i in education.schools) {
+
+    var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[i].schoolName);
+    var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+    var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+    var formattedschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].city);
+    var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+    var school = formattedschoolName + formattedschoolDegree + formattedschoolDates + formattedschoolLocation + formattedschoolMajor;
+    $("#education").append(HTMLschoolStart);
+    $("#education .education-entry").last().html(school);
+}
+
+//projects
+var projects = {
+    projects: [
+        {
+            "title": "sample project 1",
+            "dates": "2016",
+            "description": "lorem sdlfj ldfk",
+            "images": ["images/p1-1.jpg", "images/p1-2.jpg"]
+        },
+        {
+            "title": "sample project 2",
+            "dates": "2016",
+            "description": "lorem sdlfj dsafsfsa ldfk",
+            "images": ["images/p2-1.jpg", "images/p2-2.gif"]
+        }
+   ]
+};
+
+projects.display = function () {
+    for (var i in projects.projects) {
+        var project = projects.projects[i];
+        $("#projects").append(HTMLprojectStart);
+
+        var formattedprojectTitle = HTMLprojectTitle.replace("%data%", project.title);
+        var formattedprojectDates = HTMLprojectDates.replace("%data%", project.dates);
+        var formattedprojectDescription = HTMLprojectDescription.replace("%data%", project.description);
+        
+        
+        var p = formattedprojectTitle + formattedprojectDates + formattedprojectDescription;
+        if(project.images.length > 0) {
+            for (var j in project.images) {
+                var img = project.images[j];
+                var formattedprojectImage = HTMLprojectImage.replace("%data%", img);
+                p += formattedprojectImage;
+            }
+        }
+        
+        $(".project-entry:last").append(p);
+    }
+}
+
+projects.display();
+
+$("#main").append(internationalizeButton);
+
+function inName(name) {
+    console.log(name);
+    var first = name.trim().split(" ")[0];
+    var last = name.trim().split(" ")[1];
+    var f = first.slice(0, 1).toUpperCase() + first.slice(1).toLowerCase();
+    var l = last.toUpperCase();
+    return f + " " + l;
+}
+
+
+$("#mapDiv").append(googleMap);
